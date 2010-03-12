bf55b553e8b6ce41a40e738bc6d023ba030e7a0a
qxjit (David Vollbracht) <david.vollbracht@gmail.com>
2010-03-11 22:48:06 -0500

    [David] make String cooler with different lengths and more characters

--- examples/string_examples.rb
+++ examples/string_examples.rb
@@ -10,4 +10,17 @@ describe String do
     100.times { set << Arbitrary(String) }
     set.size.should >= 99
   end
+
+  it 'should generate strings of varying length' do
+    set = Set.new
+    100.times { set << Arbitrary(String).length }
+    set.size.should >= 50
+  end
+
+  it 'should use many printable characters' do
+    printable_characters = (0..255).map {|c| "" << c}.select {|s| s =~ /[[:print:]]/}
+    set = Set.new
+    100.times { set.merge Arbitrary(String).split('') }
+    set.size.should >= (printable_characters.length * 4 / 5)
+  end
 end
--- lib/rubarb/generators/string.rb
+++ lib/rubarb/generators/string.rb
@@ -1,3 +1,4 @@
+Rubarb::PrintableCharacters = (0..255).map {|c| "" << c}.select {|s| s =~ /[[:print:]]/}
 Rubarb.define(String) do
-  (0..50).map { ('a'..'z').to_a[rand(26)] }.join
+  (0..(rand 100)).map { Rubarb::PrintableCharacters[rand(Rubarb::PrintableCharacters.length)] }.join
 end
